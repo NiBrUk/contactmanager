6c376805d8e72bbaa0cea7a8ea35313abbbc4691
Kurbin Ademaj <kurbin@imbus.local>
2020-06-07 13:37:55 +0200

    Updates

---
+++ precache-manifest.10f3bdc5cb00a5f52b801fc3f3ae089f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aefe155998c094e94b1b489185da998",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "cf80e2f30d6c898c3834",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d5934e4382edc8eaf863",
+    "url": "/contactmanager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cf80e2f30d6c898c3834",
+    "url": "/contactmanager/static/js/2.b6a11e44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contactmanager/static/js/2.b6a11e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5934e4382edc8eaf863",
+    "url": "/contactmanager/static/js/main.f954312f.chunk.js"
+  },
+  {
+    "revision": "31aa565394677d9739a4",
+    "url": "/contactmanager/static/js/runtime-main.ab1f59d6.js"
+  }
+]);
